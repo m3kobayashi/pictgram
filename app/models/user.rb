7dc12800509fe35b24a4da0ed3332167dea02dd6
Miwako Kobayashi <konbuyon5884@gmail.com>
2021-01-07 09:02:35 +0000

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
     validates :email, presence: true ,format: {with: VALID_EMAIL_REGEXID}
     
     VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z.\d]{8,32}+\z/
-    validates :password, presence: true,
-    length: {minimam: 8, maximum: 32},
-    format: {with: VALID_PASSWORD_REGEX}
+    # validates :password, presence: true,
+    # length: {minimam: 8, maximum: 32},
+    validates :password, presence: true, format: {with: VALID_PASSWORD_REGEX}
         
     has_secure_password
        
